10457e2ff0bc750b9080ba324680f332183e6a82
mostafiz181 <dev.mostafizurrahman@gmail.com>
2023-04-04 23:43:30 +0600

    fixed all problem here

--- src/Component/Friends/Friends.jsx
+++ src/Component/Friends/Friends.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Friends = () => {
-    const data = useLoaderData();
-    console.log(data)
+    const friends = useLoaderData();
+    console.log(friends)
     return (
         <div>
-            <h1>This is friends data page</h1>
+            <h1>This is friends : {friends.length}</h1>
         </div>
     );
 };
